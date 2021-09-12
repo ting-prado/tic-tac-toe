a06ba9834550fee1c25e7ed25d8c98e41e249385
Ethel <ethel.prado1998@gmail.com>
2021-09-12 09:44:23 +0800

    Fnctn to draw on gameboard added

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,30 @@
-const gameboard = (() => {
+const Player = (name,sign) => {
+    const getName = () => name;
+    const getSign = () => sign;
+}
+
+const Gameboard = (() => {
     const boardArr = [];
-    const signs = ['X', 'O'];
     for(let i=0; i<=8; i++){
-        boardArr.push(i);
+        boardArr.push(null);
     }
 
-    const gameboard = document.querySelectorAll('.box');
-    const updateGameboard = (() => {
+    const gridboxes = document.querySelectorAll('.box');
+    const updateGameboard = () => {
         for(let i=0; i<=8; i++){
-            gameboard[i].textContent = boardArr[i];
+            gridboxes[i].textContent = boardArr[i];
         }
-    })(); 
+    }
+
+    const drawBoard = e => {
+        let index = e.target.id.slice(3,5);
+        boardArr[index] = 'X';
+        updateGameboard();
+    }
+
+    gridboxes.forEach(box => {
+        box.addEventListener('click', drawBoard);
+    });
+    
+    return {gridboxes, updateGameboard, drawBoard}
 })();
